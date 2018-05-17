fc2d120b5c974b7c814784e02a58b78243b53e56
pradip <pradipshrestha718@gmail.com>
2018-05-17 07:38:33 +0545

    Initial Commitz

---
+++ client/src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+
+import logo from './logo.svg';
+
+import './App.css';
+
+class App extends Component {
+  state = {
+    response: '',
+    movie :''
+  };
+
+  componentDidMount() {
+    console.log('ComponentDidMount');
+    this.callApi()
+      .then(
+        res => this.setState({ response: res.name+ ' '+res.express })
+        
+      )
+      .catch(err => console.log(err));
+      fetch('http://facebook.github.io/react-native/movies.json').
+      then((Response)=>Response.json()).
+      then((findresponse)=>
+      {
+          console.log(findresponse);
+          this.setState({
+            movie: findresponse.title
+          })
+      })
+  }
+  constructor(){
+    super();
+    console.log('constructor');
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">{this.state.response}</p>
+        <p className="App-intro">{this.state.movie}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
